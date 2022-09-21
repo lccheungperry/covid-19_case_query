2d4150cbd105acf42181e80c29ff4655cf88fa78
Perry Cheung <cyberlchung@gmail.com>
2022-09-21 20:36:38 +0800

    delete unused method

--- COVID-19_cases_query/Program.cs
+++ COVID-19_cases_query/Program.cs
@@ -165,50 +165,6 @@ public partial class FileMeta
 
     public Guid covidCasesByDayID { get; set; }
     public CovidCasesByDay? covidCasesByDay { get; set; }
-
-    public void CopyProperties(FileMeta destination)
-    {
-        // If any this null throw an exception
-        if (this == null || destination == null)
-            throw new Exception("Source or/and Destination Objects are null");
-        // Getting the Types of the objects
-        Type typeDest = destination.GetType();
-        Type typeSrc = this.GetType();
-
-        // Iterate the Properties of the source instance and  
-        // populate them from their desination counterparts  
-        PropertyInfo[] srcProps = typeSrc.GetProperties();
-        foreach (PropertyInfo srcProp in srcProps)
-        {
-            if (!srcProp.CanRead)
-            {
-                continue;
-            }
-            PropertyInfo targetProperty = typeDest.GetProperty(srcProp.Name);
-            if (targetProperty == null)
-            {
-                continue;
-            }
-            if (!targetProperty.CanWrite)
-            {
-                continue;
-            }
-            if (targetProperty.GetSetMethod(true) != null && targetProperty.GetSetMethod(true).IsPrivate)
-            {
-                continue;
-            }
-            if ((targetProperty.GetSetMethod().Attributes & MethodAttributes.Static) != 0)
-            {
-                continue;
-            }
-            if (!targetProperty.PropertyType.IsAssignableFrom(srcProp.PropertyType))
-            {
-                continue;
-            }
-            // Passed all tests, lets set the value
-            targetProperty.SetValue(destination, srcProp.GetValue(this, null), null);
-        }
-    }
 }
 
 public partial class CovidCasesByDay
